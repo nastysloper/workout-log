f39845d27f56b9e09a2416c3d83e5cfef6d86be8
Rich Vogt <richvogtjr@gmail.com>
2014-09-07 20:10:55 -0500

    update styling, get images to work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,8 @@
 module ApplicationHelper
 
   # Returns the full title on a per-page basis
-  # Hartl tutorial...
   def full_title(page_title)
-    base_title = "Ruby on Rials Tutorial Sample App"
+    base_title = "Workout App"
     if page_title.empty?
       base_title
     else
